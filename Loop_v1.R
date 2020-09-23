5d8a063cc54b6dee2e55887b0a54b267b057c4ec
Your Name <you@example.com>
2020-09-23 11:48:57 +0200

    changes 23.09.20

--- Loop_v1.R
+++ Loop_v1.R
@@ -3,6 +3,17 @@ setwd("C:/Users/Sven Kohler/Desktop/UZH")
 library(knitr)
 library(markdown)
 library(rmarkdown)
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+library(gridExtra)
+library(grid)
+library(lattice)
+library(kableExtra)
+library(scales)
+
+# load functions file
+source("functions_report.R")
 
 # loading data
 library(readstata13)
@@ -16,7 +27,14 @@ WikiDaily_mod$wikiId <- as.factor(WikiDaily_mod$wikiId)
 levels(WikiDaily_mod$wikiId) <- c(1:5880)
 WikiDaily_mod <- WikiDaily_mod[,1:5]
 
+# setting the name of the first column of the factor returns for later merging
+colnames(FactorReturns)[1] <- "date"
+
 x <- 1:3
+
+#define days
+days_ret <-  365 #calender days
+days_vola <- 252 #average number of trading days in a year
 
 
 for(i in x){
