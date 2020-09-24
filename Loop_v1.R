502ff4c3ef73dea6fb678955939c87ff92090557
Your Name <you@example.com>
2020-09-24 13:09:58 +0200

    Changes 24.09.20

--- Loop_v1.R
+++ Loop_v1.R
@@ -23,9 +23,9 @@ WikiDaily <- read.dta13("WikiDaily.dta")
 
 # manipulating data
 WikiDaily_mod <- WikiDaily
-WikiDaily_mod$wikiId <- as.factor(WikiDaily_mod$wikiId)
-levels(WikiDaily_mod$wikiId) <- c(1:5880)
 WikiDaily_mod <- WikiDaily_mod[,1:5]
+WikiDaily_mod$wiki_nr <- as.factor(WikiDaily_mod$wikiId)
+levels(WikiDaily_mod$wiki_nr) <- c(1:5880) # previous inspection: found 5880 different IDs.
 
 # setting the name of the first column of the factor returns for later merging
 colnames(FactorReturns)[1] <- "date"
@@ -38,17 +38,17 @@ days_vola <- 252 #average number of trading days in a year
 
 
 for(i in x){
-  #get individual WikiId
-  WikiDaily_Id <- WikiDaily_mod[WikiDaily_mod$wikiId==i,]
+  #get dataframe og individual WikiId
+  WikiDaily_Id <- WikiDaily_mod[WikiDaily_mod$wiki_nr==i,]
   #extract date
   start_date <- head(WikiDaily_Id$date, 1)
   end_date <- tail(WikiDaily_Id$date, 1)
+  #extract real WikiID
+  ID <- head(WikiDaily_Id$wikiId, 1)
   
-  rmarkdown::render(input = "C:/Users/Sven Kohler/Desktop/UZH/Feedbackreport2_3_loop.Rmd", 
+  rmarkdown::render(input = "C:/Users/Sven Kohler/Desktop/UZH/R project/UZH_Feedbackreport/Feedbackreport2_3_loop.Rmd", 
                     output_format = "pdf_document",
                     output_file = paste("test_report_", i,"_", Sys.Date(), ".pdf", sep=''),
                     output_dir = "C:/Users/Sven Kohler/Desktop/UZH/LoopReports",
-                    params = list(date = paste(as.character(start_date),as.character(end_date), sep="-")))
+                    params = list(date = paste(as.character(start_date),as.character(end_date), sep="-"), Id = ID))
 }
-
-# TestTest
