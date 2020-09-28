67f4f4bb8ab876bc96526d5a8d4bf6cad881e4be
Your Name <you@example.com>
2020-09-28 23:39:34 +0200

    changes

--- sortino.r
+++ sortino.r
@@ -4,6 +4,21 @@ sortino_ratio <- function(){
   
 }
 
-downward_vola <- function(){
-  
+downward_vola <- function(list_of_returns, treshold = list("zero", "average", "user_defined"), user_treshold, raw_ret = TRUE){
+  cut_off_returns <- elem_min(list_of_returns)
+  return(sqrt(mean(cut_off_returns**2)))
 }
+
+elem_min <- function(list, threshold=0){
+  new_list <- c(0)*length(list)
+  for(i in 1:length(list)){
+    min <- min(list[i], threshold)
+    new_list[i] <- min
+  }
+  return(new_list)
+}
+
+
+elem_min(x, threshold = 0)
+
+downward_vola(x)
